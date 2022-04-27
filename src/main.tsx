e529c1b41c2704eebfa290d03988498b164604bf
safdar-azeem <safdarazeem00@gmail.com>
2022-04-27 23:10:00 +0000

    Add main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import App from './App'
+import './assets/sass/main.scss'
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom/client'
+import FallBackLoader from './components/reusable/FallBackLoader'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+   <React.StrictMode>
+      <Suspense fallback={<FallBackLoader />}>
+         <App />
+      </Suspense>
+   </React.StrictMode>
+)
