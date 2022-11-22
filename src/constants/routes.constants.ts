b6e8343623646ae0cf3fb16acfcf8172483a7b15
safdar-azeem <safdarazeem00@gmail.com>
2022-11-23 04:21:07 +0500

    Add more routes constants

--- src/constants/routes.constants.ts
+++ src/constants/routes.constants.ts
@@ -1,3 +1,9 @@
 export enum AppRoutes {
 	Home = '/',
+	PaletteGenerator = '/',
+	ColorToner = 'color-toner',
+	SolidColors = 'solid-colors',
+	ContrastChecker = 'contrast-checker',
+	ColorConverter = 'color-converter',
+	Collection = 'collection',
 }
