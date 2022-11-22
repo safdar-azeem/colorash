a3ec841a6678036be03766f3131f5d07aa297845
safdar-azeem <safdarazeem00@gmail.com>
2022-11-23 04:20:35 +0500

    Add PaletteGenerator Route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,19 @@ import { lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import { AppRoutes } from '../constants/routes.constants'
 
+const Root = lazy(() => import('../Root'))
 const Home = lazy(() => import('../pages/Home'))
+const PaletteGenerator = lazy(() => import('../pages/PaletteGenerator'))
 
 export const routes = createBrowserRouter([
 	{
-		path: AppRoutes.Home,
-		element: <Home />,
+		path: '/',
+		element: <Root />,
+		children: [
+			{
+				path: AppRoutes.Home,
+				element: <PaletteGenerator />,
+			},
+		],
 	},
 ])
