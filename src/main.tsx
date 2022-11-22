f06ffa6d9dd111bf906993dfe81f82ee4f49c474
safdar-azeem <safdarazeem00@gmail.com>
2022-11-22 20:48:27 +0500

    Add MantineProvider and NotificationsProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { MantineProvider } from '@mantine/core'
+import { NotificationsProvider } from '@mantine/notifications'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/css/main.css'
 import './assets/css/theme.css'
+import FallBackLoader from './components/base/FallBackLoader'
+import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<Suspense fallback={<FallBackLoader />}>
+			<MantineProvider
+				withGlobalStyles
+				withNormalizeCSS
+				theme={theme}>
+				<NotificationsProvider
+					position='top-right'
+					autoClose={2000}>
+					<App />
+				</NotificationsProvider>
+			</MantineProvider>
+		</Suspense>
 	</React.StrictMode>
 )
