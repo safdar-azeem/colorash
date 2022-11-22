185290a7a7b8baae82c51bd4abaf53a6110127b7
safdar-azeem <safdarazeem00@gmail.com>
2022-11-22 19:43:39 +0500

    remove sass file imprort

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './assets/sass/main.scss'
+import './assets/css/main.css'
+import './assets/css/theme.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
