b96dcb5b4b6b8d1c214e8ccd73c87c6593e4b3cb
safdar-azeem <safdarazeem00@gmail.com>
2022-11-23 04:23:47 +0500

    remove theme.css

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/css/main.css'
-import './assets/css/theme.css'
 import FallBackLoader from './components/base/FallBackLoader'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
