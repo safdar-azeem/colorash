10237edf43a39174ec3374bea1d12253df1c8d15
safdar-azeem <safdarazeem00@gmail.com>
2022-11-23 04:20:55 +0500

    Remove mantine provider

--- src/main.tsx
+++ src/main.tsx
@@ -6,21 +6,11 @@ import App from './App'
 import './assets/css/main.css'
 import './assets/css/theme.css'
 import FallBackLoader from './components/base/FallBackLoader'
-import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<Suspense fallback={<FallBackLoader />}>
-			<MantineProvider
-				withGlobalStyles
-				withNormalizeCSS
-				theme={theme}>
-				<NotificationsProvider
-					position='top-right'
-					autoClose={2000}>
-					<App />
-				</NotificationsProvider>
-			</MantineProvider>
+			<App />
 		</Suspense>
 	</React.StrictMode>
 )
