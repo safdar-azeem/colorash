9be862c92861c57a86e07ce490089d689cc809a9
safdar-azeem <safdarazeem00@gmail.com>
2022-11-22 19:43:29 +0500

    remove component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,10 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppRoutes } from '../constants/routes.constants'
 
 const Home = lazy(() => import('../pages/Home'))
-const Components = lazy(() => import('../pages/Components'))
 
 export const routes = createBrowserRouter([
 	{
 		path: AppRoutes.Home,
 		element: <Home />,
-	},
-	{
-		path: AppRoutes.Components,
-		element: <Components />,
 	},
 ])
