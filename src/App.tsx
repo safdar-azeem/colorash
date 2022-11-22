ea14aa73fe855550a82b37b2fe2f065f1fc731a8
safdar-azeem <safdarazeem00@gmail.com>
2022-11-22 16:16:47 +0500

    Add RouterProvider and pass routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './routes'
+
 const App = () => {
-	return <div>App</div>
+	return (
+		<div>
+			<RouterProvider
+				router={routes}
+				fallbackElement={<div />}
+			/>
+		</div>
+	)
 }
 
 export default App
