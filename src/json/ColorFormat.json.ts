049cf6d935e7d0604db4fdf6c5d08dfbea9f3959
safdar-azeem <safdarazeem00@gmail.com>
2022-11-26 20:17:39 +0500

    Add colorFormat json

---
+++ src/json/ColorFormat.json.ts
@@ -0,0 +1,31 @@
+import { DropdownOptions } from '../components/base/form/Dropdown'
+
+const colorFormatOptions: DropdownOptions[] = [
+	{
+		key: '1',
+		label: 'Hex',
+		value: 'hex',
+	},
+	{
+		key: '2',
+		label: 'HSL',
+		value: 'hsl',
+	},
+	{
+		key: '3',
+		label: 'HSLA',
+		value: 'hsla',
+	},
+	{
+		key: '4',
+		label: 'RGB',
+		value: 'rgb',
+	},
+	{
+		key: '5',
+		label: 'RGBA',
+		value: 'rgba',
+	},
+]
+
+export default colorFormatOptions
