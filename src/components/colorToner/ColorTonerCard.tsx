f29d5e14f93df455e37b2909fcfcb4b7716b3262
safdar-azeem <safdarazeem00@gmail.com>
2022-11-26 20:21:51 +0500

    Add colorTonerCard component

---
+++ src/components/colorToner/ColorTonerCard.tsx
@@ -0,0 +1,42 @@
+import { colord } from 'colord'
+import { useMemo } from 'react'
+import { useAppDispatch } from '../../hooks/store'
+import { addToast } from '../../store/slices/toast.slice'
+import Button from '../base/form/Button'
+
+interface CardColorProps {
+	color: any
+}
+
+const ColorTonerCard = ({ color }: CardColorProps) => {
+	const dispatch = useAppDispatch()
+	const colorValue = useMemo(() => {
+		return colord(color.parsed).toHex()
+	}, [color])
+
+	const isLight = useMemo(() => {
+		return colord(colorValue).isLight()
+	}, [colorValue])
+
+	const copyToClipboard = () => {
+		navigator.clipboard.writeText(colorValue)
+		dispatch(addToast(`${colorValue} copied to clipboard`))
+	}
+
+	return (
+		<div
+			className='p-2 rounded-xl h-[120px] flex items-center gap-x-2 justify-center text-nfs-1'
+			style={{ backgroundColor: colorValue }}>
+			<p className={` ${!isLight ? 'text-white' : 'text-black'}`}>{colorValue}</p>
+			<Button
+				variant='ghost'
+				size='sm'
+				iconColor={!isLight ? 'text-gray-300' : 'text-gray-700'}
+				rightIcon='material-symbols:content-copy'
+				onClick={copyToClipboard}
+			/>
+		</div>
+	)
+}
+
+export default ColorTonerCard
