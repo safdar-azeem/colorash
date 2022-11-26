e1cca8b093b579b186151b3a35a4e55ca9cce47f
safdar-azeem <safdarazeem00@gmail.com>
2022-11-26 20:18:31 +0500

    Add react-redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App'
 import './assets/sass/main.scss'
 import FallBackLoader from './components/base/FallBackLoader'
+import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<Suspense fallback={<FallBackLoader />}>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</Suspense>
 	</React.StrictMode>
 )
