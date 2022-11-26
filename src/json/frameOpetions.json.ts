19fc0e11cbb2e6703a67229d84bebf768883f7b1
safdar-azeem <safdarazeem00@gmail.com>
2022-11-26 20:18:02 +0500

    Add frameoption json

---
+++ src/json/frameOpetions.json.ts
@@ -0,0 +1,24 @@
+import { DropdownOptions } from './../components/base/form/Dropdown'
+const frameOptions: DropdownOptions[] = [
+	{
+		key: '1',
+		label: 'Website',
+		value: 'website',
+	},
+	{
+		key: '2',
+		label: 'Mobile',
+		value: 'mobile',
+	},
+	{
+		key: '3',
+		label: 'Brochure',
+		value: 'brochure',
+	},
+	{
+		key: '4',
+		label: 'Social Media',
+		value: 'social-media',
+	},
+]
+export default frameOptions
