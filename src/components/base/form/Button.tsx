8ea571b25f21becc58a4dfb487edb910df7ad135
safdar-azeem <safdarazeem00@gmail.com>
2022-11-26 20:19:17 +0500

    Add Button component

--- src/components/base/form/Button.tsx
+++ src/components/base/form/Button.tsx
@@ -1,18 +1,9 @@
 import { Icon } from '@iconify/react'
+import { TButtonVariant, TSize } from '../../../types/tailwind.types'
 
 interface ButtonProps {
-	variant?:
-		| 'primary'
-		| 'secondary'
-		| 'ghost'
-		| 'outline'
-		| 'accent'
-		| 'warning'
-		| 'success'
-		| 'error'
-		| 'outline'
-		| 'disabled'
-	size?: 'xs' | 'sm' | 'md' | 'lg'
+	variant?: TButtonVariant
+	size?: TSize
 	color?: string
 	isBlock?: boolean
 	isCircle?: boolean
@@ -24,6 +15,7 @@ interface ButtonProps {
 	rightIcon?: string
 	iconSize?: string
 	text?: string
+	label?: string
 	onClick?: () => void
 	iconColor?: string
 }
@@ -41,34 +33,38 @@ const Button = ({
 	rightIcon,
 	iconSize,
 	text,
+	label,
 	onClick,
 	color,
 	iconColor,
 }: ButtonProps) => {
 	return (
-		<button
-			onClick={onClick}
-			style={{ color: color }}
-			className={`btn gap-2
+		<div>
+			{label && <label className='label'>{label}</label>}
+			<button
+				onClick={onClick}
+				style={{ color: color }}
+				className={`btn gap-2
 			${(leftIcon || rightIcon) && !text && 'btn-square'} btn-${variant} btn-${size} ${
-				isBlock && 'btn-block'
-			} ${isCircle && 'btn-circle'} ${isSuare && 'btn-square'} ${isAnimated && 'loading'} ${
-				isLoading && 'btn-loading'
-			} ${isGlass && 'btn-glass'}`}>
-			{leftIcon && !isAnimated && (
-				<Icon
-					icon={leftIcon}
-					className={`${iconSize || 'icon ' + size} ${iconColor} `}
-				/>
-			)}
-			{text && <span className='btn-text'>{text}</span>}
-			{rightIcon && !isAnimated && (
-				<Icon
-					icon={rightIcon}
-					className={`${iconSize || 'icon ' + size} ${iconColor} `}
-				/>
-			)}
-		</button>
+					isBlock && 'btn-block'
+				} ${isCircle && 'btn-circle'} ${isSuare && 'btn-square'} ${isAnimated && 'loading'} ${
+					isLoading && 'btn-loading'
+				} ${isGlass && 'btn-glass'}`}>
+				{leftIcon && !isAnimated && (
+					<Icon
+						icon={leftIcon}
+						className={`${iconSize || 'icon ' + size} ${iconColor} `}
+					/>
+				)}
+				{text && <span className='btn-text'>{text}</span>}
+				{rightIcon && !isAnimated && (
+					<Icon
+						icon={rightIcon}
+						className={`${iconSize || 'icon ' + size} ${iconColor} `}
+					/>
+				)}
+			</button>
+		</div>
 	)
 }
 
