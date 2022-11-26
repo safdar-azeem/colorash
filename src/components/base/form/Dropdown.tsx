fea7f444ae18a81321c047f56acb2b0f3547d5bb
safdar-azeem <safdarazeem00@gmail.com>
2022-11-26 20:19:09 +0500

    Add dropdown component

---
+++ src/components/base/form/Dropdown.tsx
@@ -0,0 +1,186 @@
+import { Icon } from '@iconify/react'
+import React, { useCallback, useMemo, useRef, useState } from 'react'
+import useClickOutside from '../../../hooks/useClickOutside'
+import { TButtonVariant, TRadius, TShadow, TSize } from '../../../types/tailwind.types'
+
+export type DropdownOptions = {
+	key: string
+	label: string
+	value: string
+}
+
+interface DropdownProps {
+	options?: DropdownOptions[]
+	value?: string
+	placeholder?: string
+	label?: string
+	dropDownwidth?: number | string
+	children?: React.ReactNode
+	button?: React.ReactNode
+	minButtonWidth?: number | string
+	buttonSize?: TSize
+	withIcon?: boolean
+	icon?: string
+	iconSize?: TSize
+	iconPosition?: 'left' | 'right'
+	iconColor?: string
+	variant?: TButtonVariant
+	directionX?: 'left' | 'end' | 'none'
+	directionY?: 'top' | 'bottom' | 'none'
+	contentMargin?: string
+	contentPadding?: string
+	contentWidth?: string
+	contentHeight?: string
+	contentBorderRadius?: TRadius
+	contentBorder?: string
+	contentBoxShadow?: TShadow
+	contentBackgroundColor?: string
+	contentColor?: string
+	hover?: boolean
+	onChange?: (value: string) => void
+	search?: boolean
+	isScrollable?: boolean
+	tabIndex?: number
+}
+
+const Dropdown = ({
+	options,
+	placeholder,
+	label,
+	value,
+	dropDownwidth,
+	children,
+	button,
+	buttonSize = 'md',
+	minButtonWidth = 140,
+	withIcon,
+	icon = 'akar-icons:chevron-vertical',
+	iconSize = 'md',
+	iconPosition = 'right',
+	iconColor = 'text-gray-600',
+	variant,
+	directionY = 'bottom',
+	directionX = 'end',
+	hover,
+	contentMargin = 'mt-2',
+	contentPadding = 'p-2',
+	contentWidth = 'w-56',
+	contentHeight = 'max-h-[220px]',
+	contentBorderRadius = 'md',
+	contentBoxShadow = 'shadow',
+	contentBackgroundColor = 'bg-base-100',
+	contentColor = 'text-base-content',
+	onChange,
+	search = false,
+	isScrollable = true,
+	tabIndex = 0,
+}: DropdownProps) => {
+	const popover = useRef(null)
+	const [isOpen, toggle] = useState(false)
+
+	const close = useCallback(() => toggle(false), [])
+	const handleToggle = () => {
+		console.log('handleToggle')
+		toggle(!isOpen)
+	}
+	console.log('isOpen', isOpen)
+	useClickOutside(popover, close)
+
+	const [searchValue, setSearchValue] = React.useState('')
+	const [error, setError] = React.useState('')
+
+	const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchValue(e.target.value)
+	}
+
+	const handleOptionClick = (value: string) => onChange && onChange(value)
+
+	const filteredOptions = useMemo(() => {
+		if (!searchValue) {
+			setError('')
+			return options
+		}
+		const filtered = options?.filter((option) =>
+			option.label.toLowerCase().includes(searchValue.toLowerCase())
+		)
+		!filtered || filtered.length === 0 ? setError('No results found') : setError('')
+		return filtered
+	}, [options, searchValue])
+
+	return (
+		<div>
+			{label && <label className='label'>{label}</label>}
+			<div
+				className={`dropdown rounded-md dropdown-${directionY} dropdown-${directionX} ${
+					hover && 'dropdown-hover'
+				} dropdown-open `}
+				style={{
+					width: dropDownwidth,
+				}}>
+				{button ? (
+					<span onClick={handleToggle}>{button}</span>
+				) : (
+					<label
+						className={`btn btn-${buttonSize} btn-${variant} gap-3 justify-between `}
+						style={{ minWidth: minButtonWidth }}
+						onClick={handleToggle}>
+						{withIcon && iconPosition === 'left' && (
+							<Icon
+								icon={icon}
+								className={`icon ${iconSize} ${iconColor}`}
+							/>
+						)}
+						<span>{value || placeholder}</span>
+						{withIcon && iconPosition === 'right' && (
+							<Icon
+								icon={icon}
+								className={`icon ${iconSize} ${iconColor}`}
+							/>
+						)}
+					</label>
+				)}
+
+				{isOpen &&
+					(children ? (
+						<div
+							ref={popover}
+							className='dropdown-content'>
+							{children}
+						</div>
+					) : (
+						<div
+							ref={popover}
+							className={`dropdown-content menu flex-nowrap ${contentWidth} ${contentBackgroundColor}   ${contentBoxShadow}  rounded-${contentBorderRadius}  ${contentPadding} ${contentMargin}  `}>
+							{search && (
+								<input
+									type='text'
+									className='input input-sm mb-2 input-bordered w-full sticky top-0 z-10'
+									placeholder='Search'
+									onChange={handleSearchChange}
+								/>
+							)}
+							<ul
+								className={` ${contentColor} ${contentHeight}  ${
+									isScrollable ? 'overflow-auto' : 'max-h-max'
+								} `}>
+								{filteredOptions?.map((option) => (
+									<li onClick={() => handleOptionClick(option.value)}>
+										<a className={`${value === option.value && 'bg-primary text-white'}`}>
+											{option.label}
+										</a>
+									</li>
+								))}
+								{error && (
+									<li>
+										<a>{error}</a>
+									</li>
+								)}
+							</ul>
+						</div>
+					))}
+			</div>
+		</div>
+	)
+}
+
+export default Dropdown
