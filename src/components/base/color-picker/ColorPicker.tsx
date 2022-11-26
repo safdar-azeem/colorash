99e90614359bebdf48e07be18ef0ef0b1f63d293
safdar-azeem <safdarazeem00@gmail.com>
2022-11-26 20:19:29 +0500

    Add colorPicker component

--- src/components/base/color-picker/ColorPicker.tsx
+++ src/components/base/color-picker/ColorPicker.tsx
@@ -1,43 +1,93 @@
-import { useCallback, useMemo, useRef, useState } from 'react'
-import { HexColorPicker } from 'react-colorful'
-import useClickOutside from '../../../hooks/useClickOutSide'
+import iro from '@jaames/iro'
+import { colord } from 'colord'
+import { useEffect, useMemo, useRef, useState } from 'react'
+import { TRadius, TSize } from '../../../types/tailwind.types'
+import Dropdown from '../form/Dropdown'
 
 interface ColorPickerProps {
 	color: string
+	rounded?: TRadius
+	size?: TSize
 	handleChange?: (color: string) => void
-	rounded?: 'none' | 'sm' | 'md' | 'lg' | 'full'
-	size?: 'xs' | 'sm' | 'md' | 'lg'
 }
 
 const ColorPicker = ({ color, handleChange, rounded = 'full', size = 'md' }: ColorPickerProps) => {
-	const [colorValue, setColorValue] = useState('#e5efee')
-	const popover = useRef(null)
-	const [isOpen, toggle] = useState(false)
-
-	const close = useCallback(() => toggle(false), [])
-	useClickOutside(popover, close)
+	const ref = useRef(null)
+	var colorPicker = useRef(null)
+	const [isOpen, setIsOpen] = useState(false)
+	const [colorValue, setColorValue] = useState(color || '#e5efee')
 
 	useMemo(() => {
 		handleChange && handleChange(colorValue)
 	}, [colorValue])
 
+	useEffect(() => {
+		if (!ref.current) return
+		// @ts-ignore
+		const cp = (colorPicker.current = new iro.ColorPicker(ref.current, {
+			width: 180,
+			color: color,
+			margin: 8,
+			boxHeight: 140,
+			sliderSize: 16,
+			layout: [
+				{
+					component: iro.ui.Box,
+				},
+				{
+					component: iro.ui.Slider,
+					options: {
+						sliderType: 'hue',
+					},
+				},
+				{
+					component: iro.ui.Slider,
+					options: {
+						sliderType: 'saturation',
+					},
+				},
+				{
+					component: iro.ui.Slider,
+					options: {
+						sliderType: 'value',
+					},
+				},
+				{
+					component: iro.ui.Slider,
+					options: {
+						sliderType: 'alpha',
+					},
+				},
+			],
+		}))
+		cp.on('color:change', (color: any) => {
+			const colorFormat = color.$.a !== 1 ? 'hsla' : 'hex'
+			const colorValue = colorFormat === 'hsla' ? color.hslaString : color.hexString
+			setColorValue(colord(colorValue).toHex())
+		})
+	}, [isOpen])
+
 	return (
-		<div className='relative'>
-			<button
-				className={`btn px-0 mt-1 box ${size} rounded-${rounded} `}
-				style={{ backgroundColor: color }}
-				onClick={() => toggle(!isOpen)}></button>
-			{isOpen && (
-				<div
-					className='absolute z-10 bottom-100 left-0 mt-2'
-					ref={popover}>
-					<HexColorPicker
-						color={colorValue}
-						onChange={setColorValue}
+		<Dropdown
+			tabIndex={0}
+			button={
+				<button
+					className={`btn px-0 mt-1 box ${size} rounded-${rounded} `}
+					style={{ backgroundColor: color }}
+					onClick={() => setIsOpen(!isOpen)}></button>
+			}
+			children={
+				<div className='shadow-lg p-3 border border-gray-100 bg-white w-max rounded-xl mt-2'>
+					<div
+						id='picker'
+						className='mt-3'
+						ref={ref}
 					/>
 				</div>
-			)}
-		</div>
+			}
+			directionY='bottom'
+			directionX='none'
+		/>
 	)
 }
 
