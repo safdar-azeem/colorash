6a6b245bb67c1788807692e24cd0bcbd39a09761
safdar-azeem <safdarazeem00@gmail.com>
2022-11-26 20:17:21 +0500

    Add ToastProvider and extend colord plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { extend } from 'colord'
+import mixPlugin from 'colord/plugins/mix'
 import { RouterProvider } from 'react-router-dom'
 import FallBackLoader from './components/base/FallBackLoader'
+import ToastProvider from './components/base/ToastProvider'
 import { routes } from './routes'
+
+extend([mixPlugin])
 
 const App = () => {
 	return (
 		<>
+			<ToastProvider />
 			<RouterProvider
 				router={routes}
 				fallbackElement={<FallBackLoader />}
