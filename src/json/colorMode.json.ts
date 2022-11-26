561ee8e3e4fcb52443d5c9a9c36b1f329549e163
safdar-azeem <safdarazeem00@gmail.com>
2022-11-26 20:17:47 +0500

    Add colorMode json

---
+++ src/json/colorMode.json.ts
@@ -0,0 +1,21 @@
+import { DropdownOptions } from '../components/base/form/Dropdown'
+
+const colorModeOptions: DropdownOptions[] = [
+	{
+		key: '1',
+		label: 'Tints',
+		value: 'tints',
+	},
+	{
+		key: '2',
+		label: 'Shades',
+		value: 'shades',
+	},
+	{
+		key: '3',
+		label: 'Tones',
+		value: 'tones',
+	},
+]
+
+export default colorModeOptions
