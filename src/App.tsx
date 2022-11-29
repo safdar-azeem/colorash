95628c857a8b1016557533df00a145cbf70431dd
safdar-azeem <safdarazeem00@gmail.com>
2022-11-30 00:17:37 +0500

    Add a11yPlugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { extend } from 'colord'
+import a11yPlugin from 'colord/plugins/a11y'
 import mixPlugin from 'colord/plugins/mix'
 import namesPlugin from 'colord/plugins/names'
 import { RouterProvider } from 'react-router-dom'
@@ -6,17 +7,16 @@ import FallBackLoader from './components/base/FallBackLoader'
 import ToastProvider from './components/base/ToastProvider'
 import { routes } from './routes'
 
-extend([namesPlugin])
-extend([mixPlugin])
+extend([namesPlugin, mixPlugin, a11yPlugin])
 
 const App = () => {
 	return (
 		<>
-			<ToastProvider />
 			<RouterProvider
 				router={routes}
 				fallbackElement={<FallBackLoader />}
 			/>
+			<ToastProvider />
 		</>
 	)
 }
