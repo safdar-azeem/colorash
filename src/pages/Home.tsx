4c2fa9ac2c2a77548b487c603b873d2db9c2099b
safdar-azeem <safdarazeem00@gmail.com>
2022-12-01 15:23:41 +0500

    Update Heading style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,10 @@ const Home = () => {
 		<div className='grid  place-items-center   min-h-screen  bg-[#F5EEE1]'>
 			<section className='grid lg:grid-cols-2 gap-24 md:container'>
 				<aside>
-					<h1 className='text-fs-10 font-black'>Generate Color palettes Easily with Colorash</h1>
+					<h1 className='text-fs-10 font-black'>
+						Generate Color palettes Easily with
+						<span className='text-primary ml-2'>Colorash</span>
+					</h1>
 					<p className='my-4'>
 						Create the perfect color palette for your next project with Colorash. Attract your
 						audience with a great color design combination and make your website stand out from the
