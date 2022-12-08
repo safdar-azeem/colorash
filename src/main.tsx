e3fe95c97a20e553f54089da7b94b65b66bce5d9
safdar-azeem <safdarazeem00@gmail.com>
2022-12-08 17:32:09 +0500

    COLORS GENERATOR

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/sass/main.scss'
 import FallBackLoader from './components/reusable/FallBackLoader'
-import { inject } from '@vercel/analytics'
-inject()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
